43b35675f273f33d5510b8432c0e20fdf07cfed8
Alexandre Faustino <alexandrefaust1@hotmail.com>
2021-07-07 18:55:24 -0300

    Criação base comentários

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
-for( const element of toggle) {
-    element.addEventListener('click',()=>{
-        nav.classList.toggle('show')
-    })
+for (const element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 }
 
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
-for(const link of links){
-    link.addEventListener('click', ()=>{
-        nav.classList.remove('show')
-    })
-
+for (const link of links) {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
 }
 
 /* Madar Header quando usar scroll*/
 
-const header = document.querySelector("#header")
-const navHeight = header.offsetHeight
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY >= navHeight){
-        header.classList.add('scroll')
-    }else{
-        header.classList.remove('scroll')
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
+
+/* Testimonial slide carrosel */
+
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/* ScrollReveal */
+
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .text, #home .image,
+   #about .image, #about .text,
+   #services header, #services .card,
+   #testimonials header, #testimonials .testimonials,
+   #contact .text, #contact .links`,
+  { interval: 100 }
+);
