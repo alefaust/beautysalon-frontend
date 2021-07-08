62388ecae65deb221aeaa8e04b433466890fcb5f
Alexandre Faustino <alexandrefaust1@hotmail.com>
2021-07-07 21:31:21 -0300

    Finalizado Layout Responsivel

--- main.js
+++ main.js
@@ -17,16 +17,16 @@ for (const link of links) {
 
 /* Madar Header quando usar scroll*/
 
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", () => {
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
   }
-});
+}
 
 /* Testimonial slide carrosel */
 
@@ -53,6 +53,22 @@ scrollReveal.reveal(
    #about .image, #about .text,
    #services header, #services .card,
    #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links`,
+   #contact .text, #contact .links,
+   footer .brand, footer .social`,
   { interval: 100 }
 );
+
+/* Back-to-top */
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+window.addEventListener("scroll", () => {
+  changeHeaderWhenScroll();
+  backToTop();
+});
